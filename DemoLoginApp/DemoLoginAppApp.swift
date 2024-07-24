b110ad5ea7a479b7aeb333225be1902169c6f62e
KHIRAJ UMREDKAR <kdumredkar@gmail.com>
2024-07-24 14:19:50 +0530

    Initial Commit

---
+++ DemoLoginApp/DemoLoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoLoginAppApp.swift
+//  DemoLoginApp
+//
+//  Created by Khiraj Umredkar on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoLoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
